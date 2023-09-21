f8f5195e6d9a889de6a4d07d66a500821d07b677
Pablobaji <jbaltazar7@ucol.mxu>
2023-09-20 18:04:41 -0600

    ola

---
+++ main.js
@@ -0,0 +1,24 @@
+// let A=Pablo;
+// x = 10+10;
+// console.log (A);
+let velocidad = 95.5;
+console.log(typeof velocidad);  
+let userName = "Pablo"
+console.log (typeof userName);
+let isActive = true
+console.log (typeof isActive);
+let edad = null;
+console.log (typeof edad);
+let x = 3n**50n;
+console.log (typeof x); 
+let contraseña = Symbol ("hola");
+console.log (typeof contraseña);
+let celular =["iphone 15",10,true];
+console.log (typeof(celular));
+let celulares ={modelo: "iphone 15",ram: 8, camara:500};
+console.log (typeof celulares);
+let a = [2,4];
+let b = a;
+b.push(5);
+console.log (a);
+console.log (b);
